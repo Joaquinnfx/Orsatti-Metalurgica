ea39b54315f777f476959eff70eea868b378fa27
JoaquinC <joacomdp14@gmail.com>
2025-10-31 15:59:07 -0300

    modal de categorias, efectos visuales de todas las pags

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -76,4 +76,115 @@ document.addEventListener('click', (e) => {
   }
 });
 
+// Modal para funcionalidades de la pagina principal
+//Ocultar btn back to top al abrir modal
+// Función para crear y mostrar el modal
+function mostrarModal(titulo, descripcion, imagenes = []) {
+  const backToTopBtn = document.querySelector('.back-to-top');
+  const modalCategorias = document.getElementById('modal-categorias');
+  modalCategorias.style.display = 'flex';
 
+  const carouselHTML = crearCarousel('carouselModal', imagenes);
+
+  modalCategorias.innerHTML = `
+    <div class="modal-categorias-content">
+      <span class="cerrar">&times;</span>
+      <div class="modal-img-container">
+        ${carouselHTML}
+      </div>
+      <div class="modal-info-container">
+        <div class="modal-texto-container">
+          <h2>${titulo}</h2>
+          <p>${descripcion}</p>
+        </div>
+        <a href="https://wa.me/2235402839?text=Hola! Quiero cotizar un trabajo."
+           class="btn btn-primary mt-3"
+           target="_blank"
+           rel="noopener noreferrer">
+          Quiero contactarme
+        </a>
+      </div>
+    </div>
+  `;
+  backToTopBtn.style.display = 'none';
+  navBar.classList.add('head-oculto');
+  document.body.style.overflow = 'hidden';
+}
+
+//FN para crear Carousel
+function crearCarousel(id, imagenes = []) {
+  const slides = imagenes
+    .map(
+      (src, i) => `
+      <div class="carousel-item ${i === 0 ? 'active' : ''}">
+        <img src="${src}" class="d-block w-100" alt="Imagen ${i + 1}">
+      </div>`
+    )
+    .join('');
+
+  return `
+    <div id="${id}" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        ${slides}
+      </div>
+      <a class="carousel-control-prev" href="#${id}" role="button" data-slide="prev">
+        <span aria-hidden="true"><i class="fa-solid fa-angle-left fa-2xl" style="color:#e20d13;"></i></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="carousel-control-next" href="#${id}" role="button" data-slide="next">
+        <span aria-hidden="true"><i class="fa-solid fa-angle-right fa-2xl" style="color:#e20d13;"></i></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div>`;
+}
+
+//FN para cerrar modal
+function cerrarModalCategorias() {
+  const modalCategorias = document.getElementById('modal-categorias');
+  modalCategorias.style.display = 'none';
+  modalCategorias.innerHTML = '';
+  document.body.style.overflow = ''; // ✅ Restaura el scroll
+}
+
+// Eventos para abrir y cerrar el modal dependiendo el boton clickeado
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('cerrar')) {
+    cerrarModalCategorias();
+  }
+  if (e.target.id === 'aluminio-modal') {
+  mostrarModal(
+  'Trabajos en aluminio',
+  'Desarrollamos soluciones a medida en aluminio, un material que combina ligereza, versatilidad y resistencia a la corrosión. Gracias a sus propiedades, es ideal para la fabricación de estructuras, cerramientos, piezas industriales y proyectos de diseño que requieren precisión y estética',
+  [
+    "/assets/imgs/DSC00949.jpg",
+    "/assets/imgs/DSC00986.jpg",
+    "/assets/imgs/DSC01031.jpg",
+    "/assets/imgs/DSC00964.jpg"
+  ]
+  );
+
+  }
+  if (e.target.id === 'acero-modal') {
+    mostrarModal(
+  'Trabajos en acero',
+  'Ofrecemos servicios especializados en trabajos con acero, un material conocido por su durabilidad y resistencia. Desde la fabricación de estructuras metálicas hasta la creación de piezas personalizadas, nuestro equipo utiliza técnicas avanzadas para garantizar resultados de alta calidad en cada proyecto.',
+  [
+    "/assets/imgs/DSC00949.jpg",
+    "/assets/imgs/DSC00986.jpg",
+    "/assets/imgs/DSC01031.jpg",
+    "/assets/imgs/DSC00964.jpg"
+  ]
+  );
+  }
+  if (e.target.id === 'cyp-modal') {
+    mostrarModal('Corte y plegado',
+  'Contamos con tecnología avanzada para el corte y plegado de metales, permitiéndonos ofrecer soluciones precisas y eficientes para una amplia variedad de aplicaciones industriales y comerciales. Nuestro equipo está capacitado para manejar diferentes tipos de materiales, asegurando acabados de alta calidad y adaptados a las necesidades específicas de cada cliente.',
+  [
+    './assets/imgs/plegado-img.avif',
+    './assets/imgs/corte-img.avif',
+    './assets/imgs/plegado2-img.avif',
+    './assets/imgs/corte2-img.avif'
+  ]
+  );
+  }
+});
